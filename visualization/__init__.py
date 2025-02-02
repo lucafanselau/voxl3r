54f5f1b15ae164f653ef728ae22add519b3d3613
Luca Fanselau <luca.fanselau@outlook.com>
2025-02-02 21:35:36 +0100

    added image smearing visualization

--- visualization/__init__.py
+++ visualization/__init__.py
@@ -5,11 +5,12 @@ from . import images
 from . import occ
 from . import mesh
 from . import mast3r
+from . import smeared_images
 
-class Config(mast3r.Config, occ.Config, images.Config, mesh.Config, SmearMast3rConfig):
+class Config(mast3r.Config, occ.Config, images.Config, mesh.Config, SmearMast3rConfig, smeared_images.Config):
     pass
 
-class Visualizer(mast3r.Visualizer, images.Visualizer, occ.Visualizer, mesh.Visualizer):
+class Visualizer(mast3r.Visualizer, images.Visualizer, occ.Visualizer, mesh.Visualizer, smeared_images.Visualizer):
     """
     Main visualizer class. Inherits from all other visualizer classes.
     """
